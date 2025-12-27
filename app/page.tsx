4766b5a830d0e73c13e917c415a24fa670de91d3
propertyenhance <photo@stephenrowley.co.za>
2025-12-27 21:37:50 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Uploader from "@/components/Uploader";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background">
+      <Uploader />
+    </main>
+  );
+}
